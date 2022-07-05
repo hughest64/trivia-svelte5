de570923bca0c49683689ac6ea600bd9f469b9d5
Todd <todd@codeofthenorth.com>
2022-07-05 17:12:11 -0500

    round header game side sans exclamation points

--- src/lib/game/RoundHeader.svelte
+++ src/lib/game/RoundHeader.svelte
@@ -1,42 +1,74 @@
 <script lang="ts">
-    import { page } from '$app/stores'
-    import { activeRoundNumber, activeQuestionNumber, roundNumbers } from '$stores/event'
-  
-    const joinCode = $page.params?.joincode;
+	import { page } from '$app/stores';
+	import {
+		activeRound,
+		activeRoundNumber,
+		activeQuestionNumber,
+		currentRoundNumber,
+		roundNumbers
+	} from '$stores/event';
 
-    const handleRoundSelect = async (event: MouseEvent) => {
-        const target = <HTMLButtonElement>event.target
-        activeRoundNumber.set(Number(target.id))
-        // always reset the question when changing rounds
-        activeQuestionNumber.set(1)
-        // post to the game endpoint to set active round and question in a cookie
-        await fetch(`/game/${joinCode}`, {
-            method: 'POST',
-            body: JSON.stringify({
-                initialRoundNumber: target.id,
-                initialQuestionNumber: 1
-            })
-        })
-    }
-    
+	const joinCode = $page.params?.joincode;
+
+	$: console.log($currentRoundNumber);
+
+	const handleRoundSelect = async (event: MouseEvent) => {
+		const target = <HTMLButtonElement>event.target;
+		activeRoundNumber.set(Number(target.id));
+		// always reset the question when changing rounds
+		activeQuestionNumber.set(1);
+		// post to the game endpoint to set active round and question in a cookie
+		await fetch(`/game/${joinCode}`, {
+			method: 'POST',
+			body: JSON.stringify({
+				initialRoundNumber: target.id,
+				initialQuestionNumber: 1
+			})
+		});
+	};
 </script>
 
-<div class="container">
-    {#each $roundNumbers as num}
-        <button id="{String(num)}" on:click={handleRoundSelect}>{num}</button>
-    {/each}
+<h3>{$activeRound.title}</h3>
+
+<div class="round-selector">
+	{#each $roundNumbers as roundNum}
+		<button
+			class:active={$activeRoundNumber === roundNum}
+			class:current={$currentRoundNumber === roundNum}
+			id={String(roundNum)}
+			on:click={handleRoundSelect}
+		>
+			{roundNum}
+		</button>
+	{/each}
 </div>
 
-<style>
-    .container {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        gap: 1em;
-    }
-    button {
-        padding: .5em .75em;
-        font-size: 20px;
-        cursor: pointer;
-    }
+<style lang="scss">
+	h3 {
+		margin: 0.5em 0.25em;
+	}
+	.round-selector {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		gap: 0.25em;
+		button {
+			width: 3em;
+			height: 3em;
+			border: 2px solid var(--color-black);
+			border-radius: 0.5em;
+			font-size: 1em;
+			font-weight: bold;
+			color: var(--color-black);
+			background-color: var(--color-white);
+			cursor: pointer;
+			&.active {
+				color: var(--color-white);
+				background-color: var(--color-red);
+			}
+			&.current {
+				border-color: #6fcf97;
+			}
+		}
+	}
 </style>
--- src/routes/game/[joincode=integer]/index.svelte
+++ src/routes/game/[joincode=integer]/index.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-    import { page } from '$app/stores'
-    import { eventRounds, activeRoundNumber, activeQuestionNumber } from '$stores/event'
+    import { activeRound, activeRoundNumber, activeQuestionNumber } from '$stores/event'
     import RoundHeader from '$lib/game/RoundHeader.svelte';
     import Round from "$lib/game/Round.svelte"
 
@@ -10,29 +9,12 @@
     export let initialQuestionNumber: number;
     $: initialQuestionNumber && activeQuestionNumber.set(Number(initialQuestionNumber))
 
-    const joincode = $page.params.joincode
-
 </script>
 
-<h1>Welcome to Game {joincode}</h1>
-
-<div class="container">
-    {#if $eventRounds}
-        <RoundHeader />
-        <Round />
-    {:else}
-        <h3>Something Went Wrong!</h3>
-        <p>Please refresh the page to try and resovle the issue</p>
-    {/if}
-</div>
-
-<style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        gap: .75em;
-        max-width: 30rem;
-        margin: 5rem auto 0;
-    }
-</style>
+{#if !!$activeRound}
+    <RoundHeader />
+    <Round />
+{:else}
+    <h3>Something Went Wrong!</h3>
+    <p>Please refresh the page to try and resovle the issue</p>
+{/if}
