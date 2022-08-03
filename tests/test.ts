5532483b23d4158e0199dfde09bc1e9a99522a2b
todd <hughest64@gmail.com>
2022-08-03 17:03:46 -0500

    start of the testing adventure, bring it on!

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,19 @@
 import { expect, test } from '@playwright/test';
 
+/**
+ * TODO:
+ * figure out how to check a response code, example:
+ * hitting '/' without a valid jwt returns a 403 from the server
+ * that redirects to '/user/login' in the app. right now the test
+ * just times out because the requested resource never loads (is that right?)
+ * 
+ * maybe we should hit a test server on the api?
+ * it would be nice to configure auto starting the Django side (with test server?)
+ * when tests are ran.
+ */
+
 test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+    await page.goto('/user/login');
+
+    expect(await page.textContent('h1')).toBe('Login');
 });
