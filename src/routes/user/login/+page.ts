5b578223e6521ce227566476bc94bbbeede4f77f
Todd <todd@codeofthenorth.com>
2022-08-15 19:57:41 -0500

    95% done, login migrated, need to handle PageLoad type

--- src/routes/user/login/+page.ts
+++ src/routes/user/login/+page.ts
@@ -1,21 +1,20 @@
+import { error } from '@sveltejs/kit';
 import * as cookie from 'cookie';
 import { browser } from '$app/env';
-import { userdata, type UserData } from '$stores/user';
-import { checkStatusCode, getFetchConfig, setCsrfHeaders } from '$lib/utils';
-import type { PageLoad } from '@sveltejs/kit';
+import { getFetchConfig } from '$lib/utils';
+import type { Load } from '@sveltejs/kit';
 
 // TODO for migration: this will need to be moved to +page.ts, however...
 // we are currently running this function on the server and setting a cookie header,
 // does that mean we could run this in server.js (i.e, GET for page) and use the 
 // new setHeaders function
 
+// -or- can this move to +page.server.ts?
+
 const apiHost = import.meta.env.VITE_API_HOST;
 
-export const load: PageLoad = async ({ fetch, session }) => {
-    if (browser) {
-        throw new Error("@migration task: Migrate this return statement (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292693)");
-        return { status: 200 };
-    }
+export const load: Load = async ({ fetch, session }) => {
+    if (browser) return;
 
     const fetchConfig = getFetchConfig('GET');
     const response = await fetch(`${apiHost}/user/login/`, fetchConfig);
@@ -24,8 +23,9 @@ export const load: PageLoad = async ({ fetch, session }) => {
         const cookies = response.headers.get('set-cookie');
         const csrftoken = (cookies && cookie.parse(cookies)?.csrftoken) || '';
         session.csrftoken = csrftoken;
+
+        return;
     }
 
-    throw new Error("@migration task: Migrate this return statement (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292693)");
-    return checkStatusCode(response);
+    throw error(response.status);
 };
