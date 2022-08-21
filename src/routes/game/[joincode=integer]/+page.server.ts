c59ac7066a6778189db3a8f5240a5a5589048970
todd <hughest64@gmail.com>
2022-08-20 19:28:32 -0500

    notes

--- src/routes/game/[joincode=integer]/+page.server.ts
+++ src/routes/game/[joincode=integer]/+page.server.ts
@@ -6,6 +6,8 @@ import { getEventCookie, setEventCookie } from '$lib/utils';
 // TODO: just try to read the cookie and return the cookie data
 // no worries about current question/round, handle that in page.svelte
 export const load: PageServerLoad = async ({ params, request }) => {
+    // just use params.joincode to look up the cookie and return
+    // initialQuestionNumber initialRoundNumber
     const body = {
         initialRoundNumber: getStore(currentRoundNumber) || 1,
         initialQuestionNumber: getStore(currentQuestionNumber) || 1
@@ -15,6 +17,10 @@ export const load: PageServerLoad = async ({ params, request }) => {
     return { ...body, ...cookieData };
 };
 
-export const POST: PageServerLoad = async ({ params, request }) => {
+export const POST: PageServerLoad = async ({ /** setHeaders, */ params, request }) => {
+    // const formData = await request.formData();
+    // get the q and r
+    // make the cookie
+    // setHeders({ 'set-cookie': 'cookie-string' });
     return await setEventCookie(params, request);
 };
