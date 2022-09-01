96fc2002424a0c85c5ab90d96f406c9569ce8b0e
Todd <todd@codeofthenorth.com>
2022-09-01 18:05:53 -0500

    example of how to disconnect the socket on navigate, not my preferred option

--- src/routes/(app)/game/[joincode=integer]/megaround/+page.svelte
+++ src/routes/(app)/game/[joincode=integer]/megaround/+page.svelte
@@ -1,1 +1,13 @@
+<script lang="ts">
+    import { onDestroy } from 'svelte';
+    import { page } from '$app/stores';
+    const { socket } = $page.data;
+
+    // TODO: this works but would have to be repeated in each page
+    // and leads to lots of connects and disconnect that shouldn't be necessary
+    onDestroy(() => {
+        socket && socket.close();
+    });
+</script>
+
 <h1>Game - Megaround</h1>
