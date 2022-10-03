3b1340b432a655e102e3a10247a2961b8f8d1e2f
Todd <todd@codeofthenorth.com>
2022-10-03 06:56:49 -0500

    generic login function for testing

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,25 +1,30 @@
 import { expect } from '@playwright/test';
 import type { Page } from '@playwright/test';
 
-// TODO: make this take in an optional user/pass object
-// default this not a non "admin" user
+// TODO: use non-staff user not sample_admin
+export const defaultCredentials = {
+    username: 'sample_admin',
+    password: 'sample_admin'
+};
+
+export const login = async (page: Page, username: string, password: string, pageUrl = '/user/login'): Promise<void> => {
+    if (pageUrl) await page.goto(pageUrl);
+    await page.locator('input[name="username"]').fill(username);
+    await page.locator('input[name="password"]').fill(password);
+    await page.locator('input[value="Submit"]').click();
+};
+
 export const authRedirects = async (page: Page, pageUrl: string) => {
     await page.goto(pageUrl);
     await expect(page).toHaveTitle(/welcome/i);
-    
+
     await page.locator('text=Login/Create Account').click();
     await expect(page).toHaveTitle(/login/i);
     expect(await page.textContent('h1')).toBe('Login');
 
-    // TODO: use guest instead of sample_admin, but perhaps user info could be a parameter?
-    await page.locator('input[name="username"]').fill('sample_admin');
-    await page.locator('input[name="password"]').fill('sample_admin');
-    await page.locator('input[value="Submit"]').click();
-
+    await login(page, defaultCredentials.username, defaultCredentials.password, '');
     await expect(page).toHaveURL(pageUrl);
-
 };
-
 
 // TODO: not part of the fixture as the critera changes per page - use for separate log in tests!
 // await expect(page).toHaveTitle(/join/i);
