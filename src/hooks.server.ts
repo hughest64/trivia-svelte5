a44fa6b51cda6defaac3fac97b21b70a975eb245
Todd <todd@codeofthenorth.com>
2022-10-14 13:05:08 -0500

    cleanup

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,11 @@
 import type { Handle, HandleFetch } from '@sveltejs/kit';
 
-export const handle: Handle = async({ event, resolve }) => {
+export const handle: Handle = async ({ event, resolve }) => {
     const { cookies, params, request } = event;
     const csrftoken = cookies.get('csrftoken') || '';
     const jwt = cookies.get('jwt') || '';
-    
-    if (jwt && csrftoken) {   
+
+    if (jwt && csrftoken) {
         event.locals.fetchHeaders = {
             'content-type': 'application/json',
             cookie: request.headers.get('cookie') || '',
@@ -20,18 +20,16 @@ export const handle: Handle = async({ event, resolve }) => {
         event.locals.activeQuestionNumber = activeQuestionNumber || '';
     }
     const response = await resolve(event);
-    
+
     return response;
 };
 
 export const handleFetch: HandleFetch = async ({ event, request }) => {
-    const requestMod = new Request(request.url,
-        {
-            method: request.method,
-            headers: { ...request.headers, ...event.locals.fetchHeaders },
-            body: request.body
-        }    
-    );
+    const requestMod = new Request(request.url, {
+        method: request.method,
+        headers: { ...request.headers, ...event.locals.fetchHeaders },
+        body: request.body
+    });
 
     return fetch(requestMod);
 };
