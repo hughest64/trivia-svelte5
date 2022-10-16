ad7ea88d419baf95e55db0677acba1227d700023
todd <hughest64@gmail.com>
2022-10-16 12:00:48 -0500

    ideas for response posting

---
+++ src/routes/(app)/game/[joincode=integer]/+page.server.ts
@@ -0,0 +1,21 @@
+import { invalid } from '@sveltejs/kit';
+import { PUBLIC_WEBSOCKET_HOST as apiHost } from '$env/static/public';
+import type { Action } from './$types';
+
+const response: Action = async ({ fetch, request, params }) => {
+    const data = Object.fromEntries((await request.formData()).entries());
+
+    // we could check for a response id, if there isn't one post /create
+    // if there is one patch (put?) to /response/<id>
+    const response = await fetch(`${apiHost}/${params.joincode}/response/`, {
+        method: 'POST',
+        body: JSON.stringify(data)
+    });
+
+    if (!response.ok) {
+        const resposeData = await response.json();
+        return invalid(response.status, { error: resposeData.detail });
+    }
+};
+
+export const actions = { response };
--- src/routes/(app)/game/[joincode=integer]/Question.svelte
+++ src/routes/(app)/game/[joincode=integer]/Question.svelte
@@ -1,42 +1,57 @@
 <script lang="ts">
-    import { getContext } from 'svelte';
+    // import { getContext } from 'svelte';
+    import { enhance } from '$app/forms';
+    import { page } from '$app/stores';
     import { getStore } from '$lib/utils';
+    import type { ActionData } from './$types';
     import type { EventQuestion } from '$lib/types';
 
+    export let form: ActionData;
     export let activeRoundQuestion: string;
     export let activeQuestion: EventQuestion;
 
-    const socket: WebSocket = getContext('socket');
+    $: userData = $page.data?.user_data;
+    // const socket: WebSocket = getContext('socket');
     $: responseStore = getStore('responseData');
     $: response = $responseStore; // filtered to this r.q
     $: notsubmitted = $responseStore !== response;
 
-    const handleResponseSubmit = () => {
-        socket.send(
-            JSON.stringify({
-                type: 'team.update_response',
-                message: response
-            })
-        );
-    };
+    // const handleResponseSubmit = () => {
+    // or - we could just use client fetch here
+    //     socket.send(
+    //         JSON.stringify({
+    //             type: 'team.update_response',
+    //             message: response
+    //         })
+    //     );
+    // };
 </script>
 
 <h2>{activeRoundQuestion}</h2>
 
 <p class="question-text">{activeQuestion.text}</p>
 
-<form on:submit|preventDefault={handleResponseSubmit}>
+<!-- <form on:submit|preventDefault={handleResponseSubmit}> -->
+<!-- <form action="/response" method="post" use:enhance={({ data }) => {
+    data.set('round_question', activeRoundQuestion);
+    data.set('team_id', String(userData?.active_team_id || ''));
+}}> -->
+<form action="/response" method="post" use:enhance>
+    {#if form?.error}<p>{form.error}</p>{/if}
+    <!-- <input type="hidden" name="team_id" value={userData?.active_team_id}>
+    <input type="hidden" name="round_question" value={activeRoundQuestion}> -->
     <div class="input-element" class:notsubmitted >
-        <input name="response" type="text" bind:value={response} />
-        <label for="response">Enter Answer</label>
+        <input name="response_text" type="text" bind:value={response} />
+        <label for="response_text">Enter Answer</label>
     </div>
-    <input class="button button-red" type="submit" value="Submit" />
+    <button class="button button-red">Submit</button>
 </form>
 
 <style lang="scss">
     h2 {
         margin: 0.5em;
     }
+
     .question-text {
         padding: 0 1em;
     }
