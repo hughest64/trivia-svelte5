3b6c5efd422d8b1669d3de2a64b428c29918d8be
Todd <todd@codeofthenorth.com>
2022-10-20 08:07:41 -0500

    set up playwright config to potentially run a django test server

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,16 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-    webServer: {
-        command: 'npm run build && npm run preview',
-        port: 4173,
-        timeout: 10000
-    }
+    webServer: [
+        {
+            command: 'npm run build && npm run preview',
+            port: 4173,
+            timeout: 10000
+        }
+        // TODO: possibly run a test django server as described at
+        // https://docs.djangoproject.com/en/dev/ref/django-admin/#testserver
+    ],
+    use: { baseURL: 'http://localhost:4173' }
 };
 
 export default config;
