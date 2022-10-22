c9352484f6c436ae4c3754c8f006925966ce9608
todd <hughest64@gmail.com>
2022-10-22 08:35:49 -0500

    more tests

--- tests/playerResponse.spec.ts
+++ tests/playerResponse.spec.ts
@@ -1,27 +1,17 @@
 import { expect, test } from '@playwright/test';
 import type { Page } from '@playwright/test';
-import { login } from './utils.js';
+import { asyncTimeout, login } from './utils.js';
 
 /**
  * TODO:
  * test a player connected to a different game but a differnt team
  * - ( I think this will fail right now due to how team groups are nameed)
- *
- * not related to responses (other than loading?)
- *
- * - test arrow key for changing questions
  * - test swiping (how to do this?)
- *
- * - test input classname change on unsubmitted response
  */
 
 const gamePage = '/game/1234';
 const submissionOne = 'answer for question';
 const submissionTwo = 'a different answer';
-
-async function asyncTimeout(ms=200): Promise<ReturnType<typeof setTimeout>> {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-};
 
 test.describe('proper response handling during an event', async () => {
     let playerOnePage: Page;
@@ -52,6 +42,12 @@ test.describe('proper response handling during an event', async () => {
         await login(playerThreePage, { username: 'guest', password: 'guest' });
         await playerThreePage.goto(gamePage);
         await expect(playerThreePage).toHaveURL(gamePage);
+    });
+
+    test.afterEach(async () => {
+        await playerOnePage.goto('/user/logout');
+        await playerTwoPage.goto('/user/logout');
+        await playerThreePage.goto('/user/logout');
     });
 
     test('proper response handling', async () => {
@@ -99,6 +95,10 @@ test.beforeEach(async ({ page }) => {
     await page.goto(gamePage);
 });
 
+test.afterEach(async ({ page }) => {
+    await page.goto('/user/logout');
+});
+
 test('round question cookies work properly', async({ page }) => {
     expect(await page.textContent('h2')).toBe('1.1');
 
@@ -113,4 +113,26 @@ test('round question cookies work properly', async({ page }) => {
     await page.reload();
     expect(await page.textContent('h2')).toBe('3.4');
     
+});
+
+test('arrow keys change the active question', async ({ page }) => {
+    expect(await page.textContent('h2')).toBe('1.1');
+    await page.keyboard.press('ArrowRight');
+    await asyncTimeout(150);
+    expect(await page.textContent('h2')).toBe('1.2');
+});
+
+test('unsubmitted clss is applied properly', async ({ page }) => {
+    const responseInput = page.locator('input[name="response_text"]');
+    // expect the class not be to applied
+    expect (await responseInput.inputValue()).toBeFalsy();
+    await expect(page.locator('div.notsubmitted')).not.toBeVisible();
+    await responseInput.fill(submissionTwo);
+    // expect the class to be applied
+    await expect(page.locator('div.notsubmitted')).toBeVisible();
+    
+    await page.locator('button:has-text("Submit")').click();
+    // delay?
+    // expet the class not to be applied
+    await expect(page.locator('div.notsubmitted')).not.toBeVisible();
 });
