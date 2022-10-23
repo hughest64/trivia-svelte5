d0a10dc9524440b26bc89aafe9da1e4d2d3a14b1
Todd <todd@codeofthenorth.com>
2022-10-22 20:05:53 -0500

    start of more host things

--- src/routes/(app)/host/[joincode=integer]/Round.svelte
+++ src/routes/(app)/host/[joincode=integer]/Round.svelte
@@ -9,21 +9,12 @@
 <!-- TODO: container with the reveal all stuffz -->
 <h4>{activeRound.title}</h4>
 
-<div class="container">
-    {#each activeRound?.questions as question (question.question_number)}
-        <h3>Question {question.question_number}</h3>
-        <p>{question.text}</p>
-    {/each}
-</div>
+{#each activeRound?.questions as question (question.question_number)}
+    <h3>Question {question.question_number}</h3>
+    <p>{question.text}</p>
+{/each}
 
 <style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        gap: 1em;
-        margin-top: 1.5rem;
-    }
 
     h4 {
         margin: 2em 0.25em;
