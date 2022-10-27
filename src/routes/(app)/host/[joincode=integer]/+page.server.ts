ab5ceade2a6a75abc2b449651cd249ccaf92067d
todd <hughest64@gmail.com>
2022-10-27 16:28:43 -0500

    working on a question reveal idea

--- src/routes/(app)/host/[joincode=integer]/+page.server.ts
+++ src/routes/(app)/host/[joincode=integer]/+page.server.ts
@@ -1,11 +1,20 @@
 import type { Action } from './$types';
 
-const reveal: Action = async ({ request, url }) => {
-    console.log('hello');
-    const data = Object.fromEntries((await request.formData()).entries());
-    console.log(data);
+const reveal: Action = async ({ request }) => {
+    const data = await request.formData();
+    console.log('data', data.get('value'));
+
+    // for the case of a single question key will be r.q
+    // for the case of revealAll the key will be 'all'
+    const key = data.get('key');
+    console.log('key', key);
+
+    // post the message that a q will be revealed (no db update)
+    // async timeout
+    // post the db update
+
+    // will this return before the timeout completes?
+    return { success: true };
 };
 
-export const actions = {
-    reveal
-};
+export const actions = { reveal };
--- src/routes/(app)/host/[joincode=integer]/Question.svelte
+++ src/routes/(app)/host/[joincode=integer]/Question.svelte
@@ -1,40 +1,53 @@
 <script lang="ts">
-    import { enhance } from '$app/forms';
     import type { EventQuestion } from '$lib/types';
 
     export let roundNumber: number;
     export let question: EventQuestion;
-    let questionRevealed = false;
-    $: revealedText = questionRevealed ? 'Hide Question' : 'Reveal Question';
 
+    // TODO: this should be part the the question
+    const questionKey = `${roundNumber}.${question.question_number}`;
+    let questionRevealed = question.question_displayed;
+
+    const handleRevealQuestion = async () => {
+        questionRevealed = !questionRevealed;
+
+        const data = new FormData();
+        data.set('value', String(questionRevealed));
+        data.set('key', questionKey);
+
+        const response = await fetch('?/reveal', { method: 'POST', body: data });
+        // TODO: maybe if the response is not ok, reset the question value and set an error msg? 
+        console.log(response);
+    };
+
+    // TODO: add this data to the question? or in a cookie?
     let answerRevealed = false;
-    $: answerRevealedText = answerRevealed ? 'Click To Hide Answer' : 'Click To Answer';
 </script>
 
 <div class="host-question-panel flex-column">
-    <h3>{roundNumber}.{question.question_number}</h3>
-    <!-- <form class="flex-column" action="?/reveal" use:enhance> -->
-
-        <div class="switch-container-q">
+    <h3>{questionKey}</h3>
+    <!-- TODO: can we make this a form? that would make progressive enhancement possible -->
+    <div class="switch-container">
+        <label for={`reveal-${question.question_number}`} class="switch">
             <input
                 type="checkbox"
                 id={`reveal-${question.question_number}`}
                 name={`reveal-${question.question_number}`}
-                bind:checked={questionRevealed}
+                checked={questionRevealed}
+                on:click={handleRevealQuestion}
             />
-            <label for={`reveal-${question.question_number}`} class="switch">
-                <span class="slider" class:revealed={questionRevealed} />
-            </label>            
-            <p>{revealedText}</p>
-        </div>
-        
-        <!-- </form> -->
+            <span class="slider" />
+        </label>
+        <p>{questionRevealed ? 'Hide Question' : 'Reveal Question'}</p>
+    </div>
     <p>{question.text}</p>
 
     <!-- TODO: qustion.host_notes -->
 
     <!-- TODO: todo we could use the event cookie to store the stat of these, just need a form like seleting an active round -->
-    <button class="button button-white" on:click={() => (answerRevealed = !answerRevealed)}>{answerRevealedText}</button>
+    <button class="button button-white" on:click={() => (answerRevealed = !answerRevealed)}
+        >{answerRevealed ? 'Click To Hide Answer' : 'Click To Answer'}</button
+    >
     {#if answerRevealed}<h3>{question.answer}</h3>{/if}
 </div>
 
