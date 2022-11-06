04ffe52c0e1e5189d47f7b65f0f0d40fcea136a6
Todd <todd@codeofthenorth.com>
2022-11-06 15:24:18 -0600

    rudiments of popup component

---
+++ src/lib/Popup.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+</script>
+
+<div class="pop">
+    <div class="pop-timer">
+        <!-- if there is a counter -->
+        <div>
+            <!-- svg.pop-timer-svg > g.pop-timer-circle > circle.pop-timer-path-elapsed + path#pop-timer-path-remaining -->
+            <span class="counter-value">5</span>
+        </div>
+    </div>
+    <div class="pop-content">
+        <h4>Title</h4>
+        <h4>Message - Bold</h4>
+        <p>The Next Question Will Be Revealed in 5 seconds</p>
+        <!-- button for cases that require use interaction -->
+    </div>
+    <div class="dismiss-containter">
+        <!-- TODO: button! -->
+        <div class="dismiss"><h4>X</h4></div>
+    </div>
+</div>
+
+<style lang="scss">
+
+</style>
