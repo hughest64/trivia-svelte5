28f89b99865cefe1d2c72dfad76b453d12959252
Todd <todd@codeofthenorth.com>
2022-11-08 05:03:20 -0600

    more work on models

--- server/game/models/question.py
+++ server/game/models/question.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 QUESTION_TYPE_GENERAL_KNOWLEDGE = 0
@@ -6,12 +7,31 @@ QUESTION_TYPES = [
     (QUESTION_TYPE_GENERAL_KNOWLEDGE, 'GeneralKnowledge')
 ]
 
-class Game(models.Model):
-    pass
+# Basic immutable question, has no round or question number
+class Question(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    answer = models.CharField(max_len=255)
+    question_type = models.IntegerField(choices=QUESTION_TYPES, default=0)
+    question_url = models.CharField(max_len=255)
 
 
+# a question that belongs to a specific game, but is immutable
+class GameQuestion(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    round_number = models.IntegerField()
+    question_number = models.IntegerField()
+    key = models.CharField(max_len=3)
+
+    def __str__(self):
+        return self.key # ?
+
+# basic round information, immutable
 class GameRound(models.Model):
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
     round_number = models.IntegerField()
     title = models.CharField(max_len=128)
     description: models.CharField(max_len=128)
@@ -20,29 +40,33 @@ class GameRound(models.Model):
         return f"Round {self.round_number}: {self.title}"
 
 
-class Question(models.Model):
-    text = models.TextField()
-    answer = models.CharField(max_len=255)
-    question_type = models.IntegerField(choices=QUESTION_TYPES, default=0)
-    question_url = models.CharField(max_len=255)
-
-
-class GameQuestion(models.Model):
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    round_number = models.ForeignKey(GameRound, on_delete=models.CASCADE)
-    question_number = models.IntegerField()
-    key = models.CharField(max_len=3)
+class Game(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    block_code = models.CharField(max_length=150, default="")
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    date_used = models.DateField(default=timezone.now)
 
 
 # TODO: different file
+# a question on a trivia event, extend a game question and is mutible (q and a displayed)
 class EventQuestion(models.Model):
-    game_question = models.ForeignKey(GameQuestion, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(GameQuestion, on_delete=models.CASCADE)
     question_displayed = models.BooleanField()
     answer_displayed = models.BooleanField()
 
 
+# round for an event extends a game round with mutable boolean fields (locked and scored)
 class EventRound(models.Model):
-    round_number = models.ForeignKey(GameRound, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    round = models.ForeignKey(GameRound, on_delete=models.CASCADE)
     locked = models.BooleanField()
     scored = models.BooleanField()
+
+
+class TriviaEvent(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    date = models.DateField()
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    join_code = models.CharField(max_len=64)
