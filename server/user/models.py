702d4c2e64974dde469c2b3ebbf55bc7de9b971e
Todd <todd@codeofthenorth.com>
2022-11-09 10:59:04 -0600

    methods for pulling user/team data on the models

--- server/user/models.py
+++ server/user/models.py
@@ -5,6 +5,17 @@ from game.models import Team
 class User(AbstractUser):
     active_team_id = models.IntegerField(blank=True, null=True)
 
+    def teams_json(self):
+        return [team.to_json() for team in self.teams.all()]
+
+    def to_json(self):
+        active_team_id = self.active_team_id if self.active_team_id else None
+        return {
+            "username": self.username,
+            "active_team_id": active_team_id,
+            "teams": self.teams_json()
+        }
+
     def clean(self):
         # ensure that whenever an active id is set, the user is added to the team
         if self.active_team_id:
