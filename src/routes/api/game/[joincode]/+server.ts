f09d61ce8a07d419649469ca376bc7437c86f65c
todd <hughest64@gmail.com>
2022-11-12 16:19:16 -0600

    remove api endpoint

--- src/routes/api/game/[joincode]/+server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from './$types';
-import { PUBLIC_API_HOST as apiHost } from '$env/static/public';
-
-export const GET: RequestHandler = async ({ params }) => {
-    const apiResponse = await fetch(`${apiHost}/game/${params.joincode}`);
-    const resp = new Response(apiResponse.body);
-    return resp;
-};
