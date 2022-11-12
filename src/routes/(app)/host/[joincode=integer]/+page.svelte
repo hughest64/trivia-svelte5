ac174913efc354a3a0b4c42900d34d4da2d4a19b
todd <hughest64@gmail.com>
2022-11-12 17:24:17 -0600

    fix lock icon image issue

--- src/routes/(app)/host/[joincode=integer]/+page.svelte
+++ src/routes/(app)/host/[joincode=integer]/+page.svelte
@@ -61,15 +61,14 @@
 
 <div class="lock-container">
     <label for="round-lock" class="lock">
-        <!-- TODO: update checked when ready -->
         <input
             type="checkbox"
             name="round-lock"
             id="round-lock"
-            checked
+            bind:checked
             on:click|preventDefault={handleLockRound}
         />
-        <span />
+        <span class:checked />
     </label>
 </div>
 
@@ -108,7 +107,7 @@ this is also conditionally displayed based on lock status and the text needs upd
                 bottom: 0;
                 background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20viewBox%3D%220%200%2033.073%2033.073%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cg%20transform%3D%22translate(8.021%20-4.473)%22%3E%3Crect%20width%3D%2221.167%22%20height%3D%2216.057%22%20x%3D%22-2.068%22%20y%3D%2218.89%22%20ry%3D%222.45%22%20fill%3D%22%23413f43%22%20fill-rule%3D%22evenodd%22%2F%3E%3Cpath%20d%3D%22M-2.068%2032.497v.001c0%20.369.08.717.225%201.03h20.716c.144-.313.225-.661.225-1.03v-.001z%22%20fill%3D%22%236fcf97%22%20fill-rule%3D%22evenodd%22%2F%3E%3Cg%20transform%3D%22matrix(.29195%200%200%20.29195%20-71.867%20-7.993)%22%20fill%3D%22%236fcf97%22%3E%3Ccircle%20cx%3D%22275.324%22%20cy%3D%22117.241%22%20r%3D%226.25%22%2F%3E%3Cpath%20d%3D%22M271.337%20115.843h7.974v12.321h-7.974z%22%2F%3E%3C%2Fg%3E%3Cpath%20d%3D%22M17.715%207.353a5.953%205.953%200%2000-5.953%205.953v5.945h2.706v-6.088h.008a3.247%203.247%200%20013.24-3.105%203.247%203.247%200%20013.243%203.105h.004V15.053h2.705v-1.747a5.953%205.953%200%2000-5.953-5.953z%22%20fill%3D%22%23413f43%22%20fill-rule%3D%22evenodd%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E');
             }
-            input:checked + span {
+            .checked {
                 background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20viewBox%3D%220%200%2033.073%2033.073%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cg%20transform%3D%22matrix(1.00296%200%200%201.00296%20-.049%20-.043)%22%3E%3Crect%20width%3D%2221.104%22%20height%3D%2216.01%22%20x%3D%225.984%22%20y%3D%2214.417%22%20ry%3D%222.443%22%20fill%3D%22%23413f43%22%20fill-rule%3D%22evenodd%22%2F%3E%3Cpath%20d%3D%22M5.984%2027.984v.001c0%20.368.081.715.225%201.027h20.655c.144-.312.225-.66.225-1.027v-.001z%22%20fill%3D%22%23dc0926%22%20fill-rule%3D%22evenodd%22%2F%3E%3Cg%20transform%3D%22matrix(.2911%200%200%20.2911%20-63.608%20-12.386)%22%20fill%3D%22%23dc0926%22%3E%3Ccircle%20cx%3D%22275.324%22%20cy%3D%22117.241%22%20r%3D%226.25%22%2F%3E%3Cpath%20d%3D%22M271.337%20115.843h7.974v12.321h-7.974z%22%2F%3E%3C%2Fg%3E%3C%2Fg%3E%3Cpath%20d%3D%22M16.536%207.027a5.953%205.953%200%2000-5.953%205.953v5.945h2.707v-6.088h.008a3.247%203.247%200%20013.238-3.104%203.247%203.247%200%20013.244%203.104h.004V14.727h2.706V12.98a5.953%205.953%200%2000-5.954-5.953z%22%20fill%3D%22%23413f43%22%20fill-rule%3D%22evenodd%22%2F%3E%3C%2Fsvg%3E');
             }
         }
