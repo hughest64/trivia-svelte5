b2cc599974b4b5f23e48a09d49db195df625cf92
todd <hughest64@gmail.com>
2022-11-30 20:52:44 -0600

    rework asgi.py for prod

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -6,6 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
+import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 
@@ -15,9 +16,12 @@ from django.urls import re_path
 from game import consumers
 from user.authentication import JwtAuthMiddlewareStack
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+django_asgi_application = get_asgi_application()
+
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_application,
         "websocket": JwtAuthMiddlewareStack(
             URLRouter(
                 [
