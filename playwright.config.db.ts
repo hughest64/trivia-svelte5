87513da9000da33406dc65e818fe84327fd27275
todd <hughest64@gmail.com>
2022-12-03 16:04:31 -0600

    add a separate playwright config to run against the django test db

--- playwright.config.db.ts
+++ playwright.config.db.ts
@@ -5,11 +5,9 @@ const config: PlaywrightTestConfig = {
     timeout: 60000, // 1 minute
     webServer: [
         {
-            command: 'npm run build && npm run preview',
+            command: 'npm run build -- --mode test && npm run preview',
             port: 4173
         }
-        // TODO: possibly run a test django server as described at
-        // https://docs.djangoproject.com/en/dev/ref/django-admin/#testserver
     ],
     use: { baseURL: 'http://127.0.0.1:4173' }
 };
