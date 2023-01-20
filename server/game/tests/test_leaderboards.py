76c72ac1375de41bf5affee7170624e66017666f
todd <hughest64@gmail.com>
2023-01-19 20:10:26 -0600

    test skeleton for LeaderboardProcessor

--- server/game/tests/test_leaderboards.py
+++ server/game/tests/test_leaderboards.py
@@ -1,11 +1,32 @@
 from django.test import TestCase
 
 from game.models import *
-from game.processors import LeaderboardProcessor
+from game.processors import LeaderboardProcessor, ProcedureError
 
 
 class LeaderboardSetup(TestCase):
     fixtures = ["data-1-19-23.json"]
 
     def setUp(self) -> None:
-        pass
+        self.host_lb = Leaderboard.objects.get(event__joincode=9998)
+
+    def test_leaderboard_update(self):
+        # get all etries for 9998 event
+        # get qty
+        # validate no entries have total_pts or rank
+
+        # run the update
+        # validate pts (summed correct for 1-2 teams?)
+        # validate rank (max pts should have 1, min pts should have all().count())
+        # valiidate highes pts has rank 1
+        # validate ordering
+        return
+
+    def test_direct_update_public_leaderboard(self):
+        # validate that it raises ProcdureError
+        return
+
+    # def test_leadboard_sync(self):
+
+    # this is probably worthy of a separate class as there are many scenarios
+    # def test_tiebreakers(self):
