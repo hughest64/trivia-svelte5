6ab66597b0b698702ae974991bc397973bf763ae
todd <hughest64@gmail.com>
2023-01-21 10:34:52 -0600

    tests partially implemented

--- server/game/tests/test_leaderboards.py
+++ server/game/tests/test_leaderboards.py
@@ -8,23 +8,37 @@ class LeaderboardSetup(TestCase):
     fixtures = ["data-1-19-23.json"]
 
     def setUp(self) -> None:
-        self.host_lb = Leaderboard.objects.get(event__joincode=9998)
+        self.event = TriviaEvent.objects.get(joincode=9998)
+        self.host_lb = Leaderboard.objects.get(
+            event=self.event, leaderboard_type=LEADERBOARD_TYPE_HOST
+        )
 
     def test_leaderboard_update(self):
-        # get all etries for 9998 event
-        # get qty
-        # validate no entries have total_pts or rank
+        entries = LeaderboardEntry.objects.filter(leaderboard=self.host_lb)
+        entry_count = len(entries)
+
+        # rank should not be set and total points should be 0 for all entries
+        self.assertEqual(entries.filter(total_points=0).count(), entry_count)
+        self.assertEqual(entries.filter(rank__isnull=True).count(), entry_count)
 
         # run the update
+        LeaderboardProcessor(
+            event=self.event, leaderboard_type=LEADERBOARD_TYPE_HOST, through_round=8
+        ).update_leaderboard()
+        entries = LeaderboardEntry.objects.filter(leaderboard=self.host_lb)
         # validate pts (summed correct for 1-2 teams?)
-        # validate rank (max pts should have 1, min pts should have all().count())
-        # valiidate highes pts has rank 1
-        # validate ordering
-        return
+
+        # entries are sorted are ranked properly
+        self.assertEqual(entries.filter(rank__isnull=False).count(), entry_count)
+        self.assertEqual(entries.first().rank, 1)
+        self.assertEqual(entries.last().rank, entry_count)
+        # need to assert a <= b <= n... for all entries
 
     def test_direct_update_public_leaderboard(self):
-        # validate that it raises ProcdureError
-        return
+        with self.assertRaises(ProcedureError):
+            LeaderboardProcessor(
+                self.event, LEADERBOARD_TYPE_PUBLIC, through_round=8
+            ).update_leaderboard()
 
     # def test_leadboard_sync(self):
 
