e17abc0622f13de6215f7899ef00ca61410377c9
Todd <todd@codeofthenorth.com>
2023-02-09 17:31:29 -0600

    nice slide transition on notes container

--- src/routes/(app)/game/[joincode=integer]/Note.svelte
+++ src/routes/(app)/game/[joincode=integer]/Note.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { slide } from 'svelte/transition';
     import { page } from '$app/stores';
     import { getStore } from '$lib/utils';
     import type { ActiveEventData } from '$lib/types';
@@ -8,18 +9,18 @@
     let hidden = true;
 </script>
 
-<!-- TODO: nice transition here when revealing the input component, it's also still styled poorly -->
 <div id="notes-container" class="notes-container flex-column">
     <button class="button disabled" on:click={() => (hidden = !hidden)}>
         Notes for {activeQuestion?.key}
     </button>
-
-    <form class:hidden on:submit|preventDefault>
-        <div class="input-container">
-            <input name="note" type="text" placeholder="Add a New Note" />
-            <!-- <label for="note">Add a New Note</label> -->
-        </div>
-    </form>
+    {#if !hidden}
+        <form transition:slide|local={{ duration: 200 }} class:hidden on:submit|preventDefault>
+            <div class="input-container">
+                <!-- TODO: note icon -->
+                <input name="note" type="text" placeholder="Add a New Note" />
+            </div>
+        </form>
+    {/if}
 </div>
 
 <style lang="scss">
