16db5f1c42ada8170ff2a6ed08aa044d221b5e97
Todd <todd@codeofthenorth.com>
2023-02-09 15:17:13 -0600

    make menu respect max-width

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -8,8 +8,10 @@
 </script>
 
 {#if displayMenu}
-    <div transition:slide={{ duration: 500 }}>
-        <Menu on:click={() => (displayMenu = false)} />
+    <div class="menu-slider" transition:slide={{ duration: 500 }}>
+        <div class="menu-content">
+            <Menu on:click={() => (displayMenu = false)} />
+        </div>
     </div>
 {/if}
 
@@ -22,16 +24,24 @@
 </Stores>
 
 <style lang="scss">
-    div {
+    .menu-slider {
+        display: flex;
         position: fixed;
         top: 0;
-        height: calc(100% - 5.5em);
+        height: calc(100% - calc(var(--footer-height) + 0.5rem));
         width: 100%;
         overflow: hidden;
-        margin: 0;
+        margin: 0 auto;
+        z-index: 99;
+    }
+    .menu-content {
+        justify-self: center;
+        height: 100%;
+        width: 100%;
+        width: var(--max-continer-width);
+        margin: 0 auto;
         border-bottom-left-radius: 3em;
         border-bottom-right-radius: 3em;
         background-color: #413f43;
-        z-index: 99;
     }
 </style>
