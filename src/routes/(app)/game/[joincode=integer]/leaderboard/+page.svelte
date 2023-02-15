8b70e208a46a2e38fd4f9a5161fc5e2b9c3a6ac4
todd <hughest64@gmail.com>
2023-02-14 18:51:41 -0600

    added round selector for leaderboard, noticed a question key bug when navigating back to the game

--- src/routes/(app)/game/[joincode=integer]/leaderboard/+page.svelte
+++ src/routes/(app)/game/[joincode=integer]/leaderboard/+page.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
+    import { page } from '$app/stores';
+    import { goto } from '$app/navigation';
     import { getStore } from '$lib/utils';
-    import type { PublicLeaderboard } from '$lib/types';
+    import type { ActiveEventData, CurrentEventData, PublicLeaderboard } from '$lib/types';
     import Entry from '$lib/leaderboards/Entry.svelte';
 
     $: leaderboard = getStore<PublicLeaderboard>('publicLeaderboard');
+
+    // TODO: this is all duplicated and should be it's own component
+    const roundNumbers = $page.data?.rounds?.map((rd) => rd.round_number) || [];
+    $: activeEventData = getStore<ActiveEventData>('activeEventData');
+    $: currentEventData = getStore<CurrentEventData>('currentEventData');
+    $: joincode = $page.params?.joincode;
+
+    const handleRoundSelect = async (event: MouseEvent) => {
+        const target = <HTMLButtonElement>event.target;
+
+        $activeEventData = {
+            activeQuestionNumber: 1,
+            activeRoundNumber: Number(target.id),
+            activeQuestionKey: `${target.id}.1`
+        };
+
+        // post to the game endpoint to set active round and question in a cookie
+        await fetch('/update', {
+            method: 'POST',
+            body: JSON.stringify({ activeEventData: $activeEventData, joincode })
+        }).then(() => goto(`/game/${joincode}`));
+    };
 </script>
 
 <h1>Team Standings</h1>
@@ -14,7 +38,18 @@
 
 <!-- existing had buttons for the host to switch between leaderboards here-->
 
-<!-- round selector (links take you back to the game at that round) -->
+<div class="round-selector">
+    {#each roundNumbers as roundNum}
+        <button
+            class:active={$activeEventData.activeRoundNumber === roundNum}
+            class:current={$currentEventData.round_number === roundNum}
+            id={String(roundNum)}
+            on:click={handleRoundSelect}
+        >
+            {roundNum}
+        </button>
+    {/each}
+</div>
 
 <ul class="leaderboard-rankings">
     {#each $leaderboard.leaderboard_entries as entry}
