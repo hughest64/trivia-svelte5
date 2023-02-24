b06072ab08fa36a06e0c017d5f820b63b741a11c
todd <hughest64@gmail.com>
2023-02-23 19:09:51 -0600

    test refactor ideas

--- tests/utils.ts
+++ tests/utils.ts
@@ -3,6 +3,19 @@ import type { Browser, Page } from '@playwright/test';
 
 const api_port = process.env.API_PORT || '8000';
 
+/**
+ * test refator ideas:
+ * use repeatable authentication (file based creds with storageState)
+ * - https://playwright.dev/docs/auth#multiple-signed-in-roles
+ * integrate that with POM classes
+ * - https://playwright.dev/docs/auth#testing-multiple-roles-with-pom-fixtures
+ * consider running tests in bigger files in parallel
+ * - https://playwright.dev/docs/test-parallel
+ * must figure out a better data reset system
+ * must use data that won't be modified by users (i.e. don't user event 1234)
+ * must reduce configurations (i.e. vscode tasks, playwright configs, npm tasks)
+ */
+
 export interface TestConfig {
     username?: string;
     password?: string;
