257ac99c4036a82bb6a69ce9b99ccbafc999739d
todd <hughest64@gmail.com>
2023-02-24 18:23:58 -0600

    another concept for (hopefully) islolated auth per worker and user

--- tests/gameResponse.spec.ts
+++ tests/gameResponse.spec.ts
@@ -1,14 +1,32 @@
-import { test } from './fixtures.js';
+import { test } from '@playwright/test';
+import { PlayerGamePage } from './gamePages.js';
 import { asyncTimeout, resetEventData } from './utils.js';
 
 const submissionOne = 'answer for question';
 const submissionTwo = 'a different answer';
 
+let p1Page: PlayerGamePage;
+let p2Page: PlayerGamePage;
+let p3Page: PlayerGamePage;
+let p4Page: PlayerGamePage;
+
+// maybe just take these steps either as a method or in the constructor of the page class?
+test.beforeAll(async ({ browser }) => {
+    const p1Context = await browser.newContext({ storageState: 'authStorage.playerFile' });
+    p1Page = new PlayerGamePage(await p1Context.newPage());
+    const p2Context = await browser.newContext({ storageState: 'authStorage.playerTwoFile' });
+    p2Page = new PlayerGamePage(await p2Context.newPage());
+    const p3Context = await browser.newContext({ storageState: 'authStorage.playerThreeFile' });
+    p3Page = new PlayerGamePage(await p3Context.newPage());
+    const p4Context = await browser.newContext({ storageState: 'authStorage.playerFourFile' });
+    p4Page = new PlayerGamePage(await p4Context.newPage());
+});
+
 test.afterAll(async () => {
     await resetEventData();
 });
 
-test('responses only update for the same team on the same event', async ({ p1Page, p2Page, p3Page, p4Page }) => {
+test('responses only update for the same team on the same event', async () => {
     p1Page.joinGame('1234');
     p2Page.joinGame('1234');
     p3Page.joinGame('1234');
