c79460023ace144f7a539dd4b124f474a89ced45
Todd <todd@codeofthenorth.com>
2023-03-13 11:02:15 -0500

    tests now run against a separate built node app instead of vite preview

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,21 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
+const testcmd = `
+    npm run build:test &&\
+    ORIGIN='http://127.0.0.1:4173'\
+    HOST='127.0.0.1'\
+    PORT=4173\
+    node -r dotenv/config build_test dotenv_config_path=./.env.test
+`;
+
 const config: PlaywrightTestConfig = {
     // retries: 1,
     workers: 2,
     timeout: 30000,
     webServer: [
         {
-            command: 'npm run build:test && npm run preview:test -- --mode test',
+            // command: 'npm run build:test && npm run preview:test -- --mode test',
+            command: testcmd,
             port: 4173
         },
         {
