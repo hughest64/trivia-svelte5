d63042fb158eb5cc9b3f8816bc8b01aa6a0b1597
Todd <todd@codeofthenorth.com>
2023-03-14 05:45:55 -0500

    notes

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -17,6 +17,7 @@
     // TODO: consisder this when navigating to /score:
     // use beforeNavigate to update $activeEventData to the lowest round that has not been scored
     // that makes it it nice an easy for the host to get to gettin'
+    // for the case of "edit this rounds scores", we could look for a query param and use that instead
 </script>
 
 <nav>
