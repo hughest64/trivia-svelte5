1f3a7b6b29dc049955e2cc58b65709c18fbc5f9e
todd <hughest64@gmail.com>
2023-03-25 10:03:00 -0500

    update team password creation

--- server/game/models/team.py
+++ server/game/models/team.py
@@ -1,6 +1,7 @@
-from django.db import models
+import random
 
-from game.utils.code_generator import get_code
+from django.conf import settings
+from django.db import models
 
 
 class Team(models.Model):
@@ -23,16 +24,19 @@ class Team(models.Model):
         return self.name
 
 
-def generate_team_password(attempts=1, max_attempts=30):
+def generate_team_password(attempts=1, max_attempts=30, sep="-"):
     """
     Generate a team password from the list of available words.
     Raises StopIteration if a unique password is not generated in max_attempts.
     """
-    password = get_code()
+    password = sep.join(random.sample(settings.WORD_LISZT, 3))
+
     if password in [i.password for i in Team.objects.all()]:
         if attempts >= max_attempts:
             raise StopIteration("Cannot generate a team password, too many attempts")
 
-        return generate_team_password(attempts=attempts + 1, max_attempts=max_attempts)
+        return generate_team_password(
+            attempts=attempts + 1, max_attempts=max_attempts, sep=sep
+        )
 
     return password
