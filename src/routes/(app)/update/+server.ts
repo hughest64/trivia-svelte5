27aef9fd3c7edb2b25a0d73fe21b47601eae2bdc
Todd <todd@codeofthenorth.com>
2023-04-20 05:45:51 -0500

    update secure cookie setting

--- src/routes/(app)/update/+server.ts
+++ src/routes/(app)/update/+server.ts
@@ -1,14 +1,14 @@
 import type { RequestHandler } from './$types';
 import { env } from '$env/dynamic/private';
 
-export const POST: RequestHandler = async ({ cookies, request }) => {
+export const POST: RequestHandler = async ({ cookies, request, url }) => {
     const data = await request.json();
 
     if (data) {
         cookies.set(`event-${data.joincode}`, JSON.stringify({ ...data.activeEventData }), {
             path: '/',
             httpOnly: true,
-            secure: Boolean(env.PUBLIC_SECURE_COOKIE),
+            secure: url.protocol === 'https:',
             maxAge: Number(env.PUBLIC_COOKIE_MAX_AGE) || 60 * 60 // default of 1 hour
         });
     }
