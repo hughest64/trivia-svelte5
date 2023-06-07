939f3f9a2dec58c49eeb218d432f331b0f5495cb
Todd <todd@codeofthenorth.com>
2023-06-07 10:49:14 -0500

    refactor redirect tests

--- tests/redirects.spec.ts
+++ tests/redirects.spec.ts
@@ -1,97 +1,86 @@
 import { expect, test } from '@playwright/test';
-import { login } from './utils.js';
+import { getUserPage } from './authConfigs.js';
+import type { PlayerGamePage, HostGamePage } from './gamePages.js';
 
 const playerSelectedTeam = 'hello world';
 
-test.describe('a player cannot access host endpoints', async () => {
-    test.beforeEach(async ({ page }) => {
-        await login(page);
-    });
+let p1: PlayerGamePage;
+let host: HostGamePage;
 
-    test('non staff user accessing /host/choice redirects to team', async ({ page }) => {
-        await page.goto('/host/choice');
-        await expect(page).toHaveURL(/\/team/i);
-    });
-
-    test('non staff user accessing /host/event-setup redirects to team', async ({ page }) => {
-        await page.goto('/host/event-setup');
-        await expect(page).toHaveURL(/team/i);
-    });
-
-    test('non staff user accessing /host/1234 redirects to team', async ({ page }) => {
-        await page.goto('/host/1234');
-        await expect(page).toHaveURL(/team/i);
-    });
-
-    test('non staff user accessing /host/1234/chat redirects to team', async ({ page }) => {
-        await page.goto('/host/1234/chat');
-        await expect(page).toHaveURL(/team/i);
-    });
-
-    test('non staff user accessing /host/1234/score redirects to team', async ({ page }) => {
-        await page.goto('/host/1234/score');
-        await expect(page).toHaveURL(/team/i);
-    });
-
-    test('non staff user accessing /host/1234/leaderboard redirects to team', async ({ page }) => {
-        await page.goto('/host/1234/leaderboard');
-        await expect(page).toHaveURL(/team/i);
-    });
+test.beforeAll(async ({ browser }) => {
+    p1 = (await getUserPage(browser, 'playerOne')) as PlayerGamePage;
+    host = (await getUserPage(browser, 'host')) as HostGamePage;
 });
 
-test.describe('logged in users cannot access login routes', async () => {
-    test.beforeEach(async ({ page }) => {
-        await login(page);
-    });
-
-    test('logged in user is redirected to /team when trying to go to /user/login', async ({ page }) => {
-        await expect(page).toHaveURL(/team/i);
-        await page.goto('/user/login');
-        await expect(page).toHaveURL(/team/i);
-    });
-
-    test('logged in user is redirected to /team when trying to go to /', async ({ page }) => {
-        await expect(page).toHaveURL(/team/i);
-        await page.goto('/');
-        await expect(page).toHaveURL(/team/i);
-    });
-
-    // TODO: this is a legit fail and the code around it needs to be fixed
-    test('/team with a next query param redirects to next', async ({ page }) => {
-        await page.goto('/team?next=game/1234');
-        await page.selectOption('select#team-select', { label: playerSelectedTeam });
-        await page.locator('text=Choose This Team').click();
-
-        await expect(page).toHaveURL('/game/1234');
-    });
+test.afterAll(async () => {
+    await p1.page.context().close();
+    await host.page.context().close();
 });
 
-test.describe('tests for a player without an active team', async () => {
-    test.beforeEach(async ({ page }) => {
-        await login(page, { username: 'sample_admin', password: 'sample_admin' });
-    });
-
-    test('user with no active team is redirected to /team when trying to join a trivia event', async ({ page }) => {
-        await page.goto('/game/1234');
-        await expect(page).toHaveURL(/team/i);
-    });
-
-    test('user with no active team is redirected to /team when trying to access a leaderboard', async ({ page }) => {
-        await page.goto('/game/1234/leaderboard');
-        await expect(page).toHaveURL(/team/i);
-    });
-
-    test('user with no active team is redirected to /team when trying to access a megaround', async ({ page }) => {
-        await page.goto('/game/1234/megaround');
-        await expect(page).toHaveURL(/team/i);
-    });
-
-    test('user with no active team is redirected to /team when trying to access team chat', async ({ page }) => {
-        await page.goto('/game/1234/chat');
-        await expect(page).toHaveURL(/team/i);
-    });
+test('non staff user accessing /host/choice redirects to team', async () => {
+    await p1.page.goto('/host/choice');
+    await expect(p1.page).toHaveURL(/\/team/i);
 });
 
-// TODO:
-// test expired (or lack of) jwt when connectiong to websocket
-// - should be logged out and endup back at login
+test('non staff user accessing /host/event-setup redirects to team', async () => {
+    await p1.page.goto('/host/event-setup');
+    await expect(p1.page).toHaveURL(/team/i);
+});
+
+test('non staff user accessing /host/1234 redirects to team', async () => {
+    await p1.page.goto('/host/1234');
+    await expect(p1.page).toHaveURL(/team/i);
+});
+
+test('non staff user accessing /host/1234/chat redirects to team', async () => {
+    await p1.page.goto('/host/1234/chat');
+    await expect(p1.page).toHaveURL(/team/i);
+});
+
+test('non staff user accessing /host/1234/score redirects to team', async () => {
+    await p1.page.goto('/host/1234/score');
+    await expect(p1.page).toHaveURL(/team/i);
+});
+
+test('non staff user accessing /host/1234/leaderboard redirects to team', async () => {
+    await p1.page.goto('/host/1234/leaderboard');
+    await expect(p1.page).toHaveURL(/team/i);
+});
+
+test('logged in user is redirected to /team when trying to go to /user/login', async () => {
+    await p1.page.goto('/user/login');
+    await expect(p1.page).toHaveURL(/team/i);
+});
+
+test('logged in user is redirected to /team when trying to go to /', async () => {
+    await p1.page.goto('/');
+    await expect(p1.page).toHaveURL(/team/i);
+});
+
+test('/team with a next query param redirects to next', async () => {
+    await p1.page.goto('/team?next=game/1234');
+    await p1.page.selectOption('select#team-select', { label: playerSelectedTeam });
+    await p1.page.locator('text=Choose This Team').click();
+
+    await expect(p1.page).toHaveURL('/game/1234');
+});
+
+test('user with no active team is redirected to /team when trying to join a trivia event', async () => {
+    await host.page.goto('/game/1234');
+    await expect(host.page).toHaveURL(/team/i);
+});
+
+test('user with no active team is redirected to /team when trying to access a leaderboard', async () => {
+    await host.page.goto('/game/1234/leaderboard');
+    await expect(host.page).toHaveURL(/team/i);
+});
+
+test('user with no active team is redirected to /team when trying to access a megaround', async () => {
+    await host.page.goto('/game/1234/megaround');
+    await expect(host.page).toHaveURL(/team/i);
+});
+
+test('user with no active team is redirected to /team when trying to access team chat', async () => {
+    await host.page.goto('/game/1234/chat');
+    await expect(host.page).toHaveURL(/team/i);
+});
