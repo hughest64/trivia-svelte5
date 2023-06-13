d3673b4b2842974cd0a51f1b3ee27f5f0abab713
Todd <todd@codeofthenorth.com>
2023-06-13 08:40:27 -0500

    add active_through date field to games and use that to filter available games for host setup

--- server/game/admin.py
+++ server/game/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 
 from .models import *
 
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("title", "active_through")
+
+
 admin.site.register(Team)
 admin.site.register(Question)
 admin.site.register(QuestionAnswer)
 admin.site.register(GameQuestion)
 admin.site.register(TiebreakerQuestion)
 admin.site.register(GameRound)
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
 admin.site.register(TriviaEvent)
 admin.site.register(EventQuestionState)
 admin.site.register(EventRoundState)
