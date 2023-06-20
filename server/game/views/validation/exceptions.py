b22521db73f26a2d0261874d8188a43b31b01628
Todd <todd@codeofthenorth.com>
2023-06-20 08:11:34 -0500

    auto create team password on team creation, duplicates the functionality of creating a join code for an event

--- server/game/views/validation/exceptions.py
+++ server/game/views/validation/exceptions.py
@@ -48,6 +48,12 @@ class JoincodeError(APIException):
     default_code = "joincode_error"
 
 
+class TeamPasswordError(APIException):
+    status_code = HTTP_400_BAD_REQUEST
+    default_detail = "An error occured with the team password"
+    default_code = "team_password_error"
+
+
 class TeamNotFound(APIException):
     status_code = HTTP_400_BAD_REQUEST
     default_detail = "No Trivia Team Found"
