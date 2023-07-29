83cde3eb312d69cf7a330ecc6e74e971badb67e5
Todd <todd@codeofthenorth.com>
2023-07-28 19:30:38 -0500

    update global type handling

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,45 +1,47 @@
-/// <reference types="@sveltejs/kit" />
+import * as types from '$lib/types';
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-declare namespace App {
-    interface Locals {
-        fetchHeaders?: Record<string, string>;
-        jwt?: string;
-        validtoken?: boolean;
-        staffuser?: boolean;
-        activeRoundNumber?: string | number;
-        activeQuestionNumber?: string | number;
-        activeQuestionKey?: string;
-        websocketHost?: string;
-        updateDelay?: number;
+declare global {
+    namespace App {
+        interface Locals {
+            fetchHeaders?: Record<string, string>;
+            jwt?: string;
+            validtoken?: boolean;
+            staffuser?: boolean;
+            activeRoundNumber?: string | number;
+            activeQuestionNumber?: string | number;
+            activeQuestionKey?: string;
+            websocketHost?: string;
+            updateDelay?: number;
+        }
+        interface PageData {
+            loaderror?: string;
+            user_data?: types.UserData;
+            event_data?: types.EventData;
+            current_event_data?: types.CurrentEventData;
+            rounds?: Array<types.GameRound>;
+            questions?: Array<types.GameQuestion>;
+            tiebreaker_questions?: Array<types.GameQuestion>;
+            tiebreaker_responses?: Array<types.TiebreakerResponse>;
+            round_states?: Array<types.EventRound>;
+            question_states?: Array<types.EventQuestion>;
+            location_select_data?: Array<types.LocationSelectData>;
+            game_select_data?: Array<types.GameSelectData>;
+            game_block_data?: string[];
+            player_joined?: boolean;
+            points_adjustment_reasons?: Array<types.PointsAdjustmentReson>;
+        }
+        // interface Platform {}
+        interface Error {
+            next?: string;
+            code?: string | number;
+        }
     }
-    interface PageData {
-        loaderror?: string;
-        user_data?: import('$lib/types').UserData;
-        event_data?: import('$lib/types').EventData;
-        current_event_data?: import('$lib/types').CurrentEventData;
-        rounds?: Array<import('$lib/types').GameRound>;
-        questions?: Array<import('$lib/types').GameQuestion>;
-        tiebreaker_questions?: Array<import('$lib/types').GameQuestion>;
-        tiebreaker_responses?: Array<import('$lib/types').TiebreakerResponse>;
-        round_states?: Array<import('$lib/types').EventRound>;
-        question_states?: Array<import('$lib/types').EventQuestion>;
-        location_select_data?: Array<import('$lib/types').LocationSelectData>;
-        game_select_data?: Array<import('$lib/types').GameSelectData>;
-        game_block_data?: string[];
-        player_joined?: boolean;
-        points_adjustment_reasons?: Array<import('$lib/types').PointsAdjustmentReson>;
-    }
-    // interface Platform {}
-    interface Error {
-        next?: string;
-        code?: string | number;
-    }
-}
 
-declare namespace svelteHTML {
-    interface HTMLAttributes<T> {
-        'on:swipe'?: (event: CustomEvent) => Promise<void>;
+    namespace svelteHTML {
+        interface HTMLAttributes<T> {
+            'on:swipe'?: (event: CustomEvent) => Promise<void>;
+        }
     }
 }
