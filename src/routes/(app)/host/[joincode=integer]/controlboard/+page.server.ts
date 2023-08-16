85999fb1a841c0306e2e2a606cdcca6694589538
Todd <todd@codeofthenorth.com>
2023-08-16 15:44:31 -0500

    fix dynamic vs. static env var imports

--- src/routes/(app)/host/[joincode=integer]/controlboard/+page.server.ts
+++ src/routes/(app)/host/[joincode=integer]/controlboard/+page.server.ts
@@ -1,10 +1,10 @@
-import { PUBLIC_API_HOST } from '$env/static/public';
-import type { PageServerLoad } from './$types.js';
+import { env } from '$env/dynamic/public';
+import type { Actions, PageServerLoad } from './$types.js';
 
 export const load: PageServerLoad = async ({ locals, params, fetch }) => {
     let apiData = {};
 
-    const response = await fetch(`${PUBLIC_API_HOST}/host/${params.joincode}/tiebreaker`);
+    const response = await fetch(`${env.PUBLIC_API_HOST}/host/${params.joincode}/tiebreaker`);
     if (response.ok) {
         apiData = await response.json();
     } else {
@@ -20,7 +20,7 @@ interface TiebreakerBody {
     team_data: Array<Record<string, FormDataEntryValue>>;
 }
 
-export const actions = {
+export const actions: Actions = {
     submit_tiebreakers: async ({ request, params, fetch }) => {
         const data = Object.fromEntries(await request.formData());
         const { tied_for_rank, question_id, ...teams } = data;
@@ -31,7 +31,7 @@ export const actions = {
             body.team_data.push({ team_id, answer });
         }
 
-        const response = await fetch(`${PUBLIC_API_HOST}/host/${params.joincode}/tiebreaker`, {
+        const response = await fetch(`${env.PUBLIC_API_HOST}/host/${params.joincode}/tiebreaker`, {
             method: 'post',
             body: JSON.stringify(body)
         });
