d54c9b1a88e4dddec2e6b4e5a1fc26cac48ec08f
Todd <todd@codeofthenorth.com>
2023-10-19 05:39:31 -0500

    add change log table

--- server/game/models/__init__.py
+++ server/game/models/__init__.py
@@ -2,3 +2,4 @@ from .team import *
 from .trivia_event import *
 from .location import *
 from .response import *
+from .change_log import *
---
+++ server/game/models/change_log.py
@@ -0,0 +1,22 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class ChangeLog(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=120)
+    version = models.CharField(max_length=120)
+    notes = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def to_json(self):
+        return {
+            "date": datetime.date.isoformat(self.created_at),
+            "title": self.title,
+            "version": self.version,
+            "notes": self.notes,
+        }
