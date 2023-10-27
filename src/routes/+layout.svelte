76861d09a56d874cf568dc9323341d2f735f6466
Todd <todd@codeofthenorth.com>
2023-10-27 06:28:16 -0500

    add cotn deets as console log in the main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script>
     import '$lib/styles/app.scss';
+    import { browser } from '$app/environment';
+
+    browser &&
+        console.log(
+            `%c
+  _____        __           ___  __  __         _  __         __  __ 
+ / ___/__  ___/ /__   ___  / _/ / /_/ /  ___   / |/ /__  ____/ /_/ / 
+/ /__/ _ \\/ _  / -_) / _ \\/ _/ / __/ _ \\/ -_) /    / _ \\/ __/ __/ _ \\
+\\___/\\___/\\_,_/\\__/  \\___/_/   \\__/_//_/\\__/ /_/|_/\\___/_/  \\__/_//_/
+                                                                     
+https://codeofthenorth.com                                                                                                                                                                                                                                                                                                                                                                                                                                                
+`,
+            `
+font-size: 9px;
+font-weight: bold;
+color: rgb(112,195,159);
+`
+        );
 </script>
 
 <slot />
