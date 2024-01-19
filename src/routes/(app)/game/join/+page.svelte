8f049d866f16b1e035d0237e7a76d5e27a11784e
todd <todd@codeofthenorth.com>
2024-01-19 06:30:43 -0600

    move team info join page

--- src/routes/(app)/game/join/+page.svelte
+++ src/routes/(app)/game/join/+page.svelte
@@ -10,18 +10,32 @@
 <svelte:head><title>Trivia Mafia | Join</title></svelte:head>
 
 <main class="short">
-    <h1>Enter Game Code</h1>
-
     {#if !!userData}
-        <p>You're playing with {activeTeam?.name}</p>
+        <h1 class="team-header">Team</h1>
+        <h2>{activeTeam?.name}</h2>
+        <div class="line" />
     {/if}
+
+    <h1>Enter Game Code</h1>
 
     <form action="?/joinevent" method="POST">
         {#if form?.error}<p class="error">{form?.error}</p>{/if}
         <div class="input-container">
             <input type="text" name="joincode" id="joincode" autocapitalize="none" autocomplete="off" required />
-            <label for="joincode">Enter Code</label>
+            <label for="joincode">Game Code</label>
         </div>
         <button class="button button-primary" type="submit">Join Game!</button>
     </form>
 </main>
+
+<style lang="scss">
+    .team-header {
+        margin-bottom: 0;
+    }
+    .line {
+        height: 2px;
+        width: min(30rem, calc(100% - 1rem));
+        margin: 1rem 0 2rem;
+        background-color: var(--color-primary);
+    }
+</style>
