f497064900c55619f8f8fc8109fc7d30c09689aa
todd <todd@codeofthenorth.com>
2024-02-12 09:16:44 -0600

    another icon tweak

--- src/lib/footer/icons/MegaroundIcon.svelte
+++ src/lib/footer/icons/MegaroundIcon.svelte
@@ -2,7 +2,7 @@
     export let cls: string = '';
 </script>
 
-<svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg" class={cls}>
+<svg width="38" height="42" viewBox="0 0 38 42" fill="none" xmlns="http://www.w3.org/2000/svg" class={cls}>
     <path
         fill-rule="evenodd"
         clip-rule="evenodd"
