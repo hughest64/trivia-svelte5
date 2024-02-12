c1ff20ecfc6655633f5fbad7001eeaeae9bfc7d0
todd <todd@codeofthenorth.com>
2024-02-12 12:05:22 -0600

    recduce some css

--- src/lib/AutoRevealToggle.svelte
+++ src/lib/AutoRevealToggle.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { page } from '$app/stores';
     import { getStore } from './utils';
     import { deserialize } from '$app/forms';
     const userData = getStore('userData');
@@ -42,56 +41,11 @@
         justify-content: flex-end;
     }
     .switch {
-        position: relative;
-        display: inline-block;
-        width: 60px;
-        height: 34px;
-        input {
-            opacity: 0;
-            width: 0;
-            height: 0;
-        }
         .slider {
-            position: absolute;
-            cursor: pointer;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
             background-color: var(--color-primary);
-            -webkit-transition: 0.4s;
-            transition: 0.4s;
-            &:before {
-                position: absolute;
-                content: '';
-                height: 26px;
-                width: 26px;
-                left: 4.5px;
-                bottom: 4.5px;
-                background-color: white;
-                -webkit-transition: 0.4s;
-                transition: 0.4s;
-            }
-            &.round {
-                border-radius: 34px;
-            }
-
-            &.round:before {
-                border-radius: 50%;
-            }
         }
         input:checked + .slider {
             background-color: var(--color-current);
-        }
-
-        input:focus + .slider {
-            box-shadow: 0 0 1px var(--color-current);
-        }
-
-        input:checked + .slider:before {
-            -webkit-transform: translateX(26px);
-            -ms-transform: translateX(26px);
-            transform: translateX(26px);
         }
     }
 </style>
