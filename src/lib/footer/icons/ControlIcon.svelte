41cb090ef4a1704466102d970bcfa00a2ec0909f
todd <todd@codeofthenorth.com>
2024-02-12 06:43:51 -0600

    update the scoring icon

--- src/lib/footer/icons/ControlIcon.svelte
+++ src/lib/footer/icons/ControlIcon.svelte
@@ -2,10 +2,10 @@
     export let cls: string = '';
 </script>
 
-<svg width="41" height="47" viewBox="0 0 41 47" fill="none" xmlns="http://www.w3.org/2000/svg" class={cls}>
-    <rect x="4" width="4.5" height="47" rx="2.5" />
-    <rect x="18" width="4.55" height="47" rx="2.5" />
-    <rect x="32" width="4.55" height="47" rx="2.5" />
+<svg width="41" height="42" viewBox="0 0 41 42" fill="none" xmlns="http://www.w3.org/2000/svg" class={cls}>
+    <rect x="4" width="4.5" height="42" rx="2.5" />
+    <rect x="18" width="4.55" height="42" rx="2.5" />
+    <rect x="32" width="4.55" height="42" rx="2.5" />
     <circle cx="6.5" cy="24.5" r="5.5" />
     <circle cx="20.5" cy="34.5" r="5.5" />
     <circle cx="34.5" cy="15.5" r="5.5" />
