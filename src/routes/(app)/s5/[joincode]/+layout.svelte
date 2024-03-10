a6d22e72e250b5cf48c3b815ca7aeed53e503fe7
todd <todd@codeofthenorth.com>
2024-03-10 14:11:34 -0500

    add websocket component

--- src/routes/(app)/s5/[joincode]/+layout.svelte
+++ src/routes/(app)/s5/[joincode]/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+    import Socket from './Socket.svelte';
+
     export let data: App.PageData;
     const { round_states, question_states, current_event_data } = data;
     console.log(round_states);
 </script>
 
-<slot />
+<Socket>
+    <slot />
+</Socket>
