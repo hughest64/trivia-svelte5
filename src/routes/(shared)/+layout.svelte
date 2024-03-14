88b74f577ab0bea28fd69165e19c9bb15e51a516
todd <todd@codeofthenorth.com>
2024-03-14 05:07:19 -0500

    functioning UserState

--- src/routes/(shared)/+layout.svelte
+++ src/routes/(shared)/+layout.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    import { UserState, getState, createState } from './stores.svelte';
-    $: data = $page.data;
-    $: console.log(data.user_data);
-
-    const userState = createState('userState', data.user_data);
+    import { createState } from './stores.svelte';
+    const data = $page.data;
+    const userState = createState('userState', data?.user_data);
 </script>
 
 <slot />
--- src/routes/(shared)/stores.svelte.ts
+++ src/routes/(shared)/stores.svelte.ts
@@ -1,31 +1,31 @@
 import { getContext, setContext } from 'svelte';
-import type { UserData, UserTeam } from '$lib/types';
+import type { LocationSelectData, UserData, UserTeam } from '$lib/types';
 
-export class UserState implements UserData {
+export class UserState {
     id;
     username;
     is_staff;
     is_guest;
-    auto_reveal_questions;
-    email;
-    active_team_id;
-    active_team;
+    auto_reveal_questions?: Boolean = $state();
+    email?: string = $state();
+    active_team_id?: number | null = $state();
+    active_team?: UserTeam = $state();
     teams;
-    user_is_anonymous;
-    home_location;
+    user_is_anonymous?: boolean = $state();
+    home_location?: LocationSelectData = $state();
 
     constructor(userData: UserData) {
-        this.id = $state(userData.id);
+        this.id = userData.id;
         this.username = userData.username;
         this.is_staff = userData.is_staff;
         this.is_guest = userData.is_guest;
-        this.auto_reveal_questions = $state(userData.auto_reveal_questions);
-        this.email = $state(userData.email);
-        this.active_team_id = $state(userData.active_team_id);
-        this.active_team = $state(userData.active_team);
-        this.teams = $state(userData.teams);
-        this.user_is_anonymous = $state(userData.user_is_anonymous);
-        this.home_location = $state(userData.home_location);
+        this.auto_reveal_questions = userData.auto_reveal_questions;
+        this.email = userData.email;
+        this.active_team_id = userData.active_team_id;
+        this.active_team = userData.active_team;
+        this.teams = userData.teams;
+        this.user_is_anonymous = userData.user_is_anonymous;
+        this.home_location = this.home_location;
     }
 }
 
