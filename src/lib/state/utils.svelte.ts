9654c1234bbdcd1c6c9b30407d6e69137e37e503
todd <todd@codeofthenorth.com>
2024-03-14 13:56:13 -0500

    a decent compromise on dynamci typing, keeps good type safety

--- src/lib/state/utils.svelte.ts
+++ src/lib/state/utils.svelte.ts
@@ -3,50 +3,13 @@ import { UserState } from './userState.svelte';
 import { GameState, type GameStateData } from './gameState.svelte';
 import type { UserData } from '$lib/types';
 
-// https://stackoverflow.com/questions/64527150/in-typescript-how-to-select-a-type-from-a-union-using-a-literal-type-property
-
-type UserStateParams = {
-    type: 'userState';
-    input: UserData;
-    output: UserState;
-};
-
-type GameStateParams = {
-    type: 'gameState';
-    input: GameStateData;
-    output: GameState;
-};
-
-type StateOpts = UserStateParams | GameStateParams;
-
-// this results in UserData
-type Tizype = Extract<StateOpts, { type: 'userState' }>['input'];
-
-const sm = {
-    userState: (data: UserStateParams['input']) => new UserState(data),
-    gameState: (data: GameStateParams['input']) => new GameState(data)
-};
-
-export function stateSetter(
-    key: keyof typeof sm,
-    data: Extract<StateOpts, { type: 'userState' }>['input']
-): Extract<StateOpts, { type: 'userState' }>['output'] {
-    const retData = sm[key](data);
-    return retData;
-}
-
-////////////////////////////////////////////////////////////////
-const stateMap = {
-    userState: (data: UserData) => new UserState(data),
-    gameState: (data: GameStateData) => new GameState(data)
-};
-
-export function createState<K extends keyof typeof stateMap>(key: K, data?: any) {
-    const stateData = stateMap[key](data) as ReturnType<(typeof stateMap)[K]>;
-
-    return setContext(key, stateData);
-}
-
-export function getState<K extends keyof typeof stateMap>(key: K): ReturnType<(typeof stateMap)[K]> {
+function getState<K extends keyof typeof stateManager>(key: K): ReturnType<(typeof stateManager)[K]> {
     return getContext(key);
 }
+const stateManager = {
+    userState: (data?: UserData) => setContext('userState', data && new UserState(data)),
+    gameState: (data?: GameStateData) => setContext('gameState', data && new GameState(data)),
+    getState
+};
+
+export default stateManager;
--- src/routes/(shared)/+layout.svelte
+++ src/routes/(shared)/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    import { createState, getState } from '$lib/state/utils.svelte';
-    // import type { UserData } from '$lib/types';
+    import stateManager from '$lib/state/utils.svelte';
+    const { userState, getState } = stateManager;
     const data = $page.data;
-    // console.log(data.user_data);
-    const ud = createState('userState', data.user_data);
-    const userState = getState('userState');
-    $inspect(userState?.active_team);
+
+    const hset = userState(data.user_data);
+    const hget = getState('userState');
+    // $inspect(hset?.active_team);
 </script>
 
 <slot />
